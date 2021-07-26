475f6c0139fb3e211f6c6e57796cc84bf8001f19
liyabai1 <1264843028@qq.com>
2021-07-26 19:48:07 +0800

    updata

--- src/components/HomePage/TopNavBar.vue
+++ src/components/HomePage/TopNavBar.vue
@@ -1,3 +1,29 @@
 <template>
-  <nav>顶部导航栏</nav>
+  <nav>
+    <router-link tag="div" to="findmusic" active-class="linkActive theme-red">个性推荐</router-link>
+    <router-link tag="div" to="songlist" active-class="linkActive theme-red">歌单</router-link>
+    <router-link tag="div" to="rank" active-class="linkActive theme-red">排行榜</router-link>
+    <router-link tag="div" to="newmusic" active-class="linkActive theme-red">最新音乐</router-link>
+  </nav>
 </template>
+<style lang="scss" scoped>
+nav {
+  display: flex;
+  height: 65px;
+  align-items: center;
+  margin-left: 20px;
+  div {
+    width: 100px;
+    height: 100%;
+    line-height: 65px;
+    background-color: #ffffff;
+    margin-right: 20px;
+    text-align: center;
+    cursor: pointer;
+  }
+}
+.linkActive {
+  font-weight: 1000;
+  box-sizing: border-box;
+}
+</style>
