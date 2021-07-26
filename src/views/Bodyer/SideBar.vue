20b5c2563862c12dbcc0dbbf0eb40651a184eff6
liyabai1 <1264843028@qq.com>
2021-07-26 18:30:34 +0800

    updata

--- src/views/Bodyer/SideBar.vue
+++ src/views/Bodyer/SideBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="siderBarBox">
-    <router-link tag="div" to="/home" active-class="selected">发现音乐</router-link>
-    <router-link tag="div" to="/fm" active-class="selected">私人FM</router-link>
-    <router-link tag="div" to="/mv" active-class="selected">MV</router-link>
+    <router-link tag="li" to="/home" active-class="selected">发现音乐</router-link>
+    <router-link tag="li" to="/fm" active-class="selected">私人FM</router-link>
+    <router-link tag="li" to="/mv" active-class="selected">MV</router-link>
     <div class="userSongList" v-show="hasLogin">
       <user-songlist></user-songlist>
     </div>
@@ -39,7 +39,8 @@ export default {
     background-color: #444;
     border-radius: 3px;
   }
-  & > div {
+  & > li {
+    list-style: none;
     height: 40px;
     width: 175px;
     line-height: 40px;
