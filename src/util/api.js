eed39dd12cb886b175c028c63167ae212456c532
liyabai1 <49718458+liyabai1@users.noreply.github.com>
2023-02-28 20:51:34 +0800

    更改网易云API网址

--- src/util/api.js
+++ src/util/api.js
@@ -1,2 +1,3 @@
-export const api = 'https://neteaseapi.liyabai.top'
+// export const api = 'https://neteaseapi.liyabai.top'
+export const api = 'http://cloud-music.pl-fe.cn/'
 // export const loginAPI = `${api}/login/cellphone`
